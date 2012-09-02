f64d0ebd85140b94d5bde5add0f01f5a1ca7943c
Pawit Pornkitprasan <p.pawit@gmail.com>
2012-09-02 11:30:20 +0700

    WebView: fix crash when moving cursor
    Caused by http://review.cyanogenmod.com/20731 when the cursor is
    moved while no text is selected.
    
    Change-Id: I71d49f1a16271dd0b7a0b4930f04d33a64a69b49

--- core/java/android/webkit/WebViewClassic.java
+++ core/java/android/webkit/WebViewClassic.java
@@ -7942,7 +7942,9 @@ public final class WebViewClassic implements WebViewProvider, WebViewProvider.Sc
                     syncSelectionCursors();
                 } else {
                     adjustSelectionCursors();
-                    mSelectCallback.setOpenUrlVisibility();
+                    if (mSelectCallback != null) {
+                        mSelectCallback.setOpenUrlVisibility();
+                    }
                 }
                 if (mIsCaretSelection) {
                     resetCaretTimer();
